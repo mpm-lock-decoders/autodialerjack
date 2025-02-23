d9cc8e8299ec5a876f630b39b9124a812cf508cf
Petr Kotas <petr@kotas.tech>
2025-02-23 17:36:34 +0000

    responsive header

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm install tailwindcss @tailwindcss/cli
+npm install prettier -D --save-exact
+
+git config --global user.email "petr@kotas.tech"
+git config --global user.name "Petr Kotas"
